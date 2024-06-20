1e430d8e9b247660e7c34cb8afe7d4aa5053d2ed
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-06-20 23:07:22 +0400

    ok

--- src/morse.zig
+++ src/morse.zig
@@ -1,122 +1,15 @@
-// AUTHOR: Rohan Vashisht
-// Library: Morse code generator (only for Zig).
-// This library provides different functionalities for
-// managing morse code in zig programming language.
-
 const std = @import("std");
-// declare the allocator I will use forever
+const eql = std.mem.eql;
 
-// A declared non-global hashmap for string.
-
-// TODO:
-// fn get_morse_for_chars(prov_string: []const u8) ![]const u8 {
-//     var res = "";
-//     for (prov_string) |value| {
-//         var character_upper = std.ascii.toUpper(value);
-//         var z = [_]u8{character_upper};
-//         var vale = my_hash_map.get(&z);
-//         if (vale) |vel| {
-//             var dz = [_][]const u8{ res, vel };
-//             res = try std.mem.concat(std.heap.page_allocator, u8, &dz);
-//         } else {
-//             res = try comptime std.fmt.allocPrint(std.heap.page_allocator, res, "XX"); // return this if unknown symbol found
-//         }
-//     }
-// }
-
-/// # Takes input as u8, outputs morse code as []const u8.
-/// `returns XX if unknown character is passed.`
-///
-/// `Example usage:`
-/// ```zig
-/// const std = @import("std");
-/// const morse = @import("morse.zig");
-///
-/// pub fn main() !void {
-///     try morse.init();
-///     var a_morse = morse.get_morse_from_char('A');
-///     std.debug.print("Morse code for a is: {s} \n", .{a_morse});
-/// }
-/// ```
-pub fn get_morse_from_char(character: u8) ![]const u8 {
-    const allocator = std.heap.page_allocator;
-    const character_upper = std.ascii.toUpper(character); // because dictionary only has upper case chars // converted u8 to []const u8 because dictionary only has []const u8
-    const vals = try getHashMap(allocator); // finding the value from the hash map
-    const val = vals.get(character_upper);
-    if (val) |value| { // checking if value was found in hashmap or not
-        return value; // returned the value if found
-    } else { // if not found:
-        return "XX"; // return XX if unknown symbol found
-    }
-    defer vals.deinit();
-    return "XX"; // unreachable code (dead code) whatever you call it!!!
+fn char_to_morse(key: u8) []const u8 {
+    return if (key == 'A') ".-" else if (key == 'B') "-..." else if (key == 'C') "-.-." else if (key == 'D') "-.." else if (key == 'E') "." else if (key == 'F') "..-." else if (key == 'G') "--." else if (key == 'H') "...." else if (key == 'I') ".." else if (key == 'J') ".---" else if (key == 'K') "-.-" else if (key == 'L') ".-.." else if (key == 'M') "--" else if (key == 'N') "-." else if (key == 'O') "---" else if (key == 'P') ".--." else if (key == 'Q') "--.-" else if (key == 'R') ".-." else if (key == 'S') "..." else if (key == 'T') "-" else if (key == 'U') "..-" else if (key == 'V') "...-" else if (key == 'W') ".--" else if (key == 'X') "-..-" else if (key == 'Y') "-.--" else if (key == 'Z') "--.." else if (key == '0') "-----" else if (key == '1') ".----" else if (key == '2') "..---" else if (key == '3') "...--" else if (key == '4') "....-" else if (key == '5') "....." else if (key == '6') "-...." else if (key == '7') "--..." else if (key == '8') "---.." else if (key == '9') "----." else if (key == '.') ".-.-.-" else if (key == ',') "--..--" else if (key == '?') "..--.." else if (key == '\'') ".----." else if (key == '!') "-.-.--" else if (key == '/') "-..-." else if (key == '(') "-.--." else if (key == ')') "-.--.-" else if (key == '&') ".-..." else if (key == ':') "---..." else if (key == ';') "-.-.-." else if (key == '=') "-...-" else if (key == '+') ".-.-." else if (key == '-') "-....-" else if (key == '_') "..--.-" else if (key == '\"') ".-..-." else if (key == '$') "...-..-" else if (key == '@') ".--.-." else if (key == ' ') "/" else "§";
 }
 
-// pub fn get_char_from_morse(morse_chars: u8) u8 {
-//     getHashMap().iterator();
-// }
-
-fn getHashMap(allocator:std.mem.Allocator) !std.AutoHashMap(u8, []const u8) {
-    var morse_hash_map = std.AutoHashMap(u8, []const u8).init(allocator);
-    try morse_hash_map.put('A', ".-");
-    try morse_hash_map.put('B', "-...");
-    try morse_hash_map.put('C', "-.-.");
-    try morse_hash_map.put('D', "-..");
-    try morse_hash_map.put('E', ".");
-    try morse_hash_map.put('F', "..-.");
-    try morse_hash_map.put('G', "--.");
-    try morse_hash_map.put('H', "....");
-    try morse_hash_map.put('I', "..");
-    try morse_hash_map.put('J', ".---");
-    try morse_hash_map.put('K', "-.-");
-    try morse_hash_map.put('L', ".-..");
-    try morse_hash_map.put('M', "--");
-    try morse_hash_map.put('N', "-.");
-    try morse_hash_map.put('O', "---");
-    try morse_hash_map.put('P', ".--.");
-    try morse_hash_map.put('Q', "--.-");
-    try morse_hash_map.put('R', ".-.");
-    try morse_hash_map.put('S', "...");
-    try morse_hash_map.put('T', "-");
-    try morse_hash_map.put('U', "..-");
-    try morse_hash_map.put('V', "...-");
-    try morse_hash_map.put('W', ".--");
-    try morse_hash_map.put('X', "-..-");
-    try morse_hash_map.put('Y', "-.--");
-    try morse_hash_map.put('Z', "--..");
-    try morse_hash_map.put('0', "-----");
-    try morse_hash_map.put('1', ".----");
-    try morse_hash_map.put('2', "..---");
-    try morse_hash_map.put('3', "...--");
-    try morse_hash_map.put('4', "....-");
-    try morse_hash_map.put('5', ".....");
-    try morse_hash_map.put('6', "-....");
-    try morse_hash_map.put('7', "--...");
-    try morse_hash_map.put('8', "---..");
-    try morse_hash_map.put('9', "----.");
-    try morse_hash_map.put('.', ".-.-.-");
-    try morse_hash_map.put(',', "--..--");
-    try morse_hash_map.put('?', "..--..");
-    try morse_hash_map.put('\'', ".----.");
-    try morse_hash_map.put('!', "-.-.--");
-    try morse_hash_map.put('/', "-..-.");
-    try morse_hash_map.put('(', "-.--.");
-    try morse_hash_map.put(')', "-.--.-");
-    try morse_hash_map.put('&', ".-...");
-    try morse_hash_map.put(',', "---...");
-    try morse_hash_map.put(';', "-.-.-.");
-    try morse_hash_map.put('=', "-...-");
-    try morse_hash_map.put('+', ".-.-.");
-    try morse_hash_map.put('-', "-....-");
-    try morse_hash_map.put('_', "..--.-");
-    try morse_hash_map.put('\"', ".-..-.");
-    try morse_hash_map.put('$', "...-..-");
-    try morse_hash_map.put('@', ".--.-.");
-    try morse_hash_map.put(' ', "/");
-    return morse_hash_map;
+fn morse_to_char(key: []const u8) u8 {
+    return if (eql(u8, key, ".-")) 'A' else if (eql(u8, key, "-...")) 'B' else if (eql(u8, key, "-.-.")) 'C' else if (eql(u8, key, "-..")) 'D' else if (eql(u8, key, ".")) 'E' else if (eql(u8, key, "..-.")) 'F' else if (eql(u8, key, "--.")) 'G' else if (eql(u8, key, "....")) 'H' else if (eql(u8, key, "..")) 'I' else if (eql(u8, key, ".---")) 'J' else if (eql(u8, key, "-.-")) 'K' else if (eql(u8, key, ".-..")) 'L' else if (eql(u8, key, "--")) 'M' else if (eql(u8, key, "-.")) 'N' else if (eql(u8, key, "---")) 'O' else if (eql(u8, key, ".--.")) 'P' else if (eql(u8, key, "--.-")) 'Q' else if (eql(u8, key, ".-.")) 'R' else if (eql(u8, key, "...")) 'S' else if (eql(u8, key, "-")) 'T' else if (eql(u8, key, "..-")) 'U' else if (eql(u8, key, "...-")) 'V' else if (eql(u8, key, ".--")) 'W' else if (eql(u8, key, "-..-")) 'X' else if (eql(u8, key, "-.--")) 'Y' else if (eql(u8, key, "--..")) 'Z' else if (eql(u8, key, "-----")) '0' else if (eql(u8, key, ".----")) '1' else if (eql(u8, key, "..---")) '2' else if (eql(u8, key, "...--")) '3' else if (eql(u8, key, "....-")) '4' else if (eql(u8, key, ".....")) '5' else if (eql(u8, key, "-....")) '6' else if (eql(u8, key, "--...")) '7' else if (eql(u8, key, "---..")) '8' else if (eql(u8, key, "----.")) '9' else if (eql(u8, key, ".-.-.-")) '.' else if (eql(u8, key, "--..--")) ',' else if (eql(u8, key, "..--..")) '?' else if (eql(u8, key, ".----.")) '\'' else if (eql(u8, key, "-.-.--")) '!' else if (eql(u8, key, "-..-.")) '/' else if (eql(u8, key, "-.--.")) '(' else if (eql(u8, key, "-.--.-")) ')' else if (eql(u8, key, ".-...")) '&' else if (eql(u8, key, "---...")) ':' else if (eql(u8, key, "-.-.-.")) ';' else if (eql(u8, key, "-...-")) '=' else if (eql(u8, key, ".-.-.")) '+' else if (eql(u8, key, "-....-")) '-' else if (eql(u8, key, "..--.-")) '_' else if (eql(u8, key, ".-..-.")) '\"' else if (eql(u8, key, "...-..-")) '$' else if (eql(u8, key, ".--.-.")) '@' else if (eql(u8, key, "/")) ' ' else '§';
 }
 
 test "morse" {
-    const x = try get_morse_from_char('X');
+    const x = char_to_morse('X');
     std.debug.print("{s}", .{x});
 }
