ce6407f8b850ceaa28bd1a749837c024bc306517
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-06-21 13:28:42 +0400

    ok

--- build.zig
+++ build.zig
@@ -1,14 +1,21 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    
+
     const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/zorsig.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    const morse_module = b.addModule("zorsig", .{
+        .root_source_file = b.path("src/zorsig.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    try b.modules.put(b.dupe("zorsig"), morse_module);
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
     const test_step = b.step("test", "Run unit tests");
